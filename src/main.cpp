23fd1a77dae5a518a281d66b381ff580e0357c83
gutianchang <2501838478@qq.com>
2023-12-17 23:28:29 +0800

    add vscode setting : disable cmake configure on open

--- src/main.cpp
+++ src/main.cpp
@@ -3,35 +3,21 @@
 #include <task.h>
 #include <timers.h>
 
-#if defined STM32F1
-    #include <stm32f1xx_hal.h>
+#include <stm32f7xx_hal.h>
 
-    // STM32VL-Discovery green led - PC9
-    #define LED_PORT                GPIOC
-    #define LED_PIN                 GPIO_PIN_9
-    // STM32VL-Discovery blue led - PC8
-    //#define LED_PIN                 GPIO_PIN_8
-    #define LED_PORT_CLK_ENABLE     __HAL_RCC_GPIOC_CLK_ENABLE
-#elif defined STM32F7
-    #include <stm32f7xx_hal.h>
+// PIXHAWK mini V4 blue led - PC7
+#define BLUE_LED_PORT                GPIOC
+#define BLUE_LED_PIN                 GPIO_PIN_7
+#define BLUE_LED_PORT_CLK_ENABLE     __HAL_RCC_GPIOC_CLK_ENABLE
 
-    // PIXHAWK mini V4 blue led - PC7
-    #define BLUE_LED_PORT                GPIOC
-    #define BLUE_LED_PIN                 GPIO_PIN_7
-    #define BLUE_LED_PORT_CLK_ENABLE     __HAL_RCC_GPIOC_CLK_ENABLE
+#define RED_LED_PORT                GPIOB
+#define RED_LED_PIN                 GPIO_PIN_1
+#define RED_LED_PORT_CLK_ENABLE     __HAL_RCC_GPIOB_CLK_ENABLE
 
-    #define RED_LED_PORT                GPIOB
-    #define RED_LED_PIN                 GPIO_PIN_1
-    #define RED_LED_PORT_CLK_ENABLE     __HAL_RCC_GPIOB_CLK_ENABLE
+#define GREEN_LED_PORT                GPIOC
+#define GREEN_LED_PIN                 GPIO_PIN_6
+#define GREEN_LED_PORT_CLK_ENABLE     __HAL_RCC_GPIOC_CLK_ENABLE
 
-    #define GREEN_LED_PORT                GPIOC
-    #define GREEN_LED_PIN                 GPIO_PIN_6
-    #define GREEN_LED_PORT_CLK_ENABLE     __HAL_RCC_GPIOC_CLK_ENABLE
-#endif
-
-
-
-#if defined STM32F7
 static void blinky::blueLEDBlinkTask(void *arg)
 {
     for(;;)
@@ -58,20 +44,9 @@ static void blinky::greenLEDBlinkTask(void *arg)
         HAL_GPIO_TogglePin(GREEN_LED_PORT, GREEN_LED_PIN);
     }
 }
-#else
-    static void blinky::blinkTask(void *arg)
-    {
-        for(;;)
-        {
-            vTaskDelay(500);
-            HAL_GPIO_TogglePin(LED_PORT, LED_PIN);
-        }
-    }
-#endif
 
 void blinky::init()
 {
-#if defined STM32F7
     GPIO_InitTypeDef GPIO_Config;
 
     GPIO_Config.Mode = GPIO_MODE_OUTPUT_PP;
@@ -87,19 +62,6 @@ void blinky::init()
     GPIO_Config.Pin = RED_LED_PIN;
     RED_LED_PORT_CLK_ENABLE();
     HAL_GPIO_Init(RED_LED_PORT, &GPIO_Config);
-
-#else
-    GPIO_InitTypeDef GPIO_Config;
-
-    GPIO_Config.Mode = GPIO_MODE_OUTPUT_PP;
-    GPIO_Config.Pull = GPIO_NOPULL;
-    GPIO_Config.Speed = GPIO_SPEED_FREQ_HIGH;
-
-    GPIO_Config.Pin = LED_PIN;
-
-    LED_PORT_CLK_ENABLE();
-    HAL_GPIO_Init(LED_PORT, &GPIO_Config);
-#endif
 }
 
 int main(void)
